809a7395412f7d69a51a91fe592db633d730f74d
Nick Stinemates <nick@stinemates.org>
2013-10-04 11:02:21 -0700

    Dockerized the application

--- init/first.py
+++ init/first.py
@@ -2,10 +2,20 @@ from config import properties
 
 import rethinkdb as r
 
-r.connect(properties.get('RETHINK_HOST'), properties.get('RETHINK_PORT')).repl()
+def check_db():
+    r.connect(properties.get('RETHINK_HOST'), properties.get('RETHINK_PORT')).repl()
+    
+    if 'relayr' in r.db_list().run():
+        return True
 
-r.db_create('relayr').run()
-r.db('relayr').table_create('requests').run()
+    return False
+
+
+def create_db():
+    r.connect(properties.get('RETHINK_HOST'), properties.get('RETHINK_PORT')).repl()
+
+    r.db_create('relayr').run()
+    r.db('relayr').table_create('requests').run()
 
 
     
--- web/server.py
+++ web/server.py
@@ -1,10 +1,16 @@
 from app import app
 from views import index
 from views import sav
+from init.first import check_db
+from init.first import create_db
 
 app.add_url_rule('/', 'index', index, methods=['GET'])
 app.add_url_rule('/', 'sav', sav, methods=['POST'])
 
 
 if __name__=="__main__":
+    # check to see if the relayr database is set up..
+    if not check_db():
+        create_db()
+
     app.run('0.0.0.0')
